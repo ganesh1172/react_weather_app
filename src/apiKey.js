58e67faa75790ce4a90231afed5533672c6e55e8
ganesh1172 <ganesh172595@gmail.com>
2021-01-29 23:15:07 +0530

    create search base weather app

--- src/apiKey.js
+++ src/apiKey.js
@@ -1,4 +1,16 @@
-module.exports = {
-    key = "{9f58defcd2f79d669fd0669a9d72417b}",
-    base = "https://api.openweathermap.org/data/2.5/",
-};  
+import axios from 'axios';
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "9f58defcd2f79d669fd0669a9d72417b";
+
+export const apiKey = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
+}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,25 @@
 import { useState } from 'react';
+import { apiKey } from '../apiKey';
 
 const Weather = () => {
-    const [query, setQuery] = useState("");
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState([]);
 
-    const search = () = {
-        
+    const search = async (e) => {
+        if (e.key === 'Enter') {
+            const data = await apiKey(query);
+            console.log(data);
+            setWeather(data);
+            setQuery('');
+        }
     }
 
     return (
-        <div>
-            <h1>Open Weather API</h1>
-            <form>
-                <input type="text" placeholder="Enter city name" value={query} onChange={(e) => e.target.value} />
-                <button onClick={search}>Search</button>
-            </form>
+        <div className="main-container">
+            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+            {weather.main && (
+                <h3>{weather.sys.country}</h3>
+            )}
         </div>
     )
 }
